6d2c6c60e4b0a1a3ce20910d51f3365977abc4c7
Asutosh Adhikari <50772652+asutoshad@users.noreply.github.com>
2026-04-21 21:41:42 +0545

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ const PORT = process.env.PORT || 3000;
 // 1. Serve the static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// 2. Handle SPA routing: send index.html for any request that doesn't match a file
-app.get('*', (req, res) => {
+// 2. Handle SPA routing for Express 5.x
+// Note the change from '*' to '/*' 
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
